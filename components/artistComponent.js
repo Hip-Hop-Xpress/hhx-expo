f058a6986a0ff3df620220092a3ee1c4b5420ab7
Renzo <renzol2@illinois.edu>
2020-06-04 14:41:21 -0500

    add featured artist image placeholders and styling

--- components/artistComponent.js
+++ components/artistComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Dimensions} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Dimensions, Image} from 'react-native';
 import globalStyles from '../styles/global';
 
 const ArtistComponent = props => {
@@ -7,8 +7,13 @@ const ArtistComponent = props => {
     <TouchableOpacity
       activeOpacity={0.75}
       // onPress={}
+      style={styles.touchable}
     >
       <View style={[styles.artistContainer]}>
+        <Image
+          style={styles.artistImage}
+          source={require('../assets/featured/default/default_header.jpeg')}
+        />
         <Text style={styles.artistName}>{props.name}</Text>
         <Text style={styles.artistDate}>{props.date}</Text>
       </View>
@@ -22,22 +27,39 @@ const screen = Dimensions.get('screen');
 const width = screen.width;
 const height = screen.height;
 
+const artistContainerPadding = 40;
+const artistImageHeight = height / 5;
+const artistImageWidth = width / 2 - artistContainerPadding * 1.5;
+
 const styles = StyleSheet.create({
   artistContainer: {
     width: width / 2 - 1,
     padding: 40,
+  },
+  artistImage: {
+    alignSelf: 'center',
+    width: artistImageWidth,
+    height: artistImageHeight,
+    borderRadius: 100,
+    marginBottom: 20,
   },
   artistName: {
     color: 'white',
     fontSize: 16,
     textAlign: 'center',
     textAlignVertical: 'bottom',
+    marginVertical: 5,
+    fontFamily: 'Karla-Regular',
   },
   artistDate: {
     color: 'lightgray',
     textAlign: 'center',
     textAlignVertical: 'bottom',
+    fontFamily: 'Karla-BoldItalic',
   },
+  touchable: {
+    backgroundColor: 'black',
+  }
 });
 
 export default ArtistComponent;
