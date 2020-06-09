852db5cd7af2095b8974240264fca1db1285440d
Renzo <renzol2@illinois.edu>
2020-06-09 15:44:33 -0500

    add new files for hhx variations

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-// import Navigator from './routes/homeStack'; // this is for the stack
 import BottomNavigator from './routes/bottomNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import * as Font from 'expo-font';
