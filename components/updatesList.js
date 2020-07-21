a6907ac78eace96004307935b7f10baa96fb873d
Renzo <renzol2@illinois.edu>
2020-07-20 22:59:17 -0500

    successfully fetch updates from api

--- components/updatesList.js
+++ components/updatesList.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import UpdateComponent from './updateComponent';
 import UPDATES from '../api/constants/updates';
+import fetchUpdates from '../api/endpoints/updates';
 
 const UpdatesList = props => {
-  let key = 0;
+  const [updates, setUpdates] = useState([]);
+  const promise = fetchUpdates();
 
-  const Updates = UPDATES.map(update => (
+  // Handle the promise obtained with fetching data from API
+  useEffect(() => {
+    promise
+      .then(response => {
+        console.log('Fetched updates successfully!');
+        setUpdates(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+
+        // If error occurs, set to constant
+        // TODO: give some warning to user in case error occurs
+        setUpdates(UPDATES);
+      });
+  }, []);
+
+  const Updates = updates.map(update => (
     <UpdateComponent
       title={update.title}
       body={update.body}
       date={update.date}
-      key={key++}
+      key={update.id}
       navigation={props.navigation}
     />
   ));
